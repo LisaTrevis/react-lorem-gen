70b4fb14b95b870d431b45fc1612224942f1a65d
Lisa Trevis <l_trevis@hotmail.com>
2021-01-19 10:50:16 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./data";
 
 function App() {
+  const [num, setNum] = useState(1);
+  const [paras, setParas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let number = parseInt(num);
+
+    if (num <= 10) {
+      setParas(data.slice(0, number));
+    }
+
+    if (num > 10 && num <= 20) {
+      number = number - 10;
+      setParas(data.concat(data.slice(0, number)));
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <div className="title">
+        <h2>Hipster Lorem Generator</h2>
+      </div>
+      <form className="form" onSubmit={handleSubmit}>
+        <label htmlFor="num">Number of paragraphs:</label>
+        <input
+          type="number"
+          name="num"
+          id="num"
+          value={num}
+          onChange={(e) => setNum(e.target.value)}
+          min="1"
+          max="20"
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+      <article>
+        {paras.map((para, index) => {
+          return <p key={index}>{para}</p>;
+        })}
+      </article>
+    </section>
   );
 }
 
